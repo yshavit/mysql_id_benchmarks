11708ca846421ac83b925b2a25232e43d1d2b5c1
Yuval Shavit <yankee.sierra@gmail.com>
2013-05-30 06:29:07 -0400

    connection stuff

--- src/main/java/com/yuvalshavit/mib/run/Runner.java
+++ src/main/java/com/yuvalshavit/mib/run/Runner.java
@@ -2,44 +2,61 @@ package com.yuvalshavit.mib.run;
 
 import com.yuvalshavit.mib.pk.PkFieldInsertProvider;
 import com.yuvalshavit.mib.schema.SchemaCreator;
+import com.yuvalshavit.mib.teststeps.JoinTest;
+import com.yuvalshavit.mib.teststeps.MainDeleteTest;
+import com.yuvalshavit.mib.teststeps.MainInsertTest;
+import com.yuvalshavit.mib.teststeps.MainUpdateTest;
 import com.yuvalshavit.mib.teststeps.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Runner {
   private final int nRows;
   private final int nThreads;
-  private final String connectionString;
+  private final String myqlUser;
+  private final String myqlPassword;
+  private final String myqlSchema;
   private final PkFieldInsertProvider pkFieldProvider;
   private final TestResultHandler resultHandler;
-  private final Collection<? extends Test> tests;
 
   public Runner(int nRows,
                 int nThreads,
-                String connectionString,
+                String myqlUser,
+                String myqlPassword,
+                String myqlSchema,
                 PkFieldInsertProvider pkFieldProvider,
-                TestResultHandler resultHandler,
-                Collection<? extends Test> tests) {
+                TestResultHandler resultHandler)
+  {
     this.nRows = nRows;
     this.nThreads = nThreads;
-    this.connectionString = connectionString;
+    this.myqlUser = myqlUser;
+    this.myqlPassword = myqlPassword;
+    this.myqlSchema = myqlSchema;
     this.pkFieldProvider = pkFieldProvider;
     this.resultHandler = resultHandler;
-    this.tests = tests;
   }
 
   private void run(boolean warmup) throws Exception {
     ConnectionProvider connector = new ConnectionProvider() {
       @Override
       public Connection get() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/foo_test?user=yuval&password=");
+        String connectString = String.format("jdbc:mysql://localhost/%s?user=%s&password=%s",
+          myqlSchema, myqlUser, myqlPassword);
+        return DriverManager.getConnection(connectString);
       }
     };
     new SchemaCreator(pkFieldProvider, connector, warmup).run();
+
+    List<Test> tests = new ArrayList<Test>(4);
+    tests.add(new MainInsertTest(pkFieldProvider));
+    tests.add(new MainUpdateTest(pkFieldProvider.replay()));
+    tests.add(new JoinTest());
+    tests.add(new MainDeleteTest(pkFieldProvider.replay()));
 
     for (Test test : tests) {
       SimpleExecutor executor = new SimpleExecutor();
